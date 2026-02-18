308bb8442d206ba05f40242c7f637698427eabb0
Bad3r <25513724+Bad3r@users.noreply.github.com>
2026-02-18 07:47:28 +0300

    fix(client): define tailwind v4 dark selector variant

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -3,7 +3,6 @@ import typography from '@tailwindcss/typography'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: ['selector', '[data-color-mode="dark"]'],
   theme: {
     extend: {
       colors: {
